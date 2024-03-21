ccfa8f1b8963fcbde7665dccdac18078dd294c4d
sourcewire <cyberskis@proton.me>
2024-03-20 17:50:33 -0700

    editing test titles

--- src/lib/sortPoints.test.js
+++ src/lib/sortPoints.test.js
@@ -15,7 +15,7 @@ test('sortPoints correctly sorts list of (x,y) ints', function() {
 })
 
 
-test('sortPoints correctly sorts list of (x,y) ints', function() {
+test('sortPoints correctly sorts list of (x,y) ints with larger array and different array elements', function() {
         //arrange
         const array = [{x:34, y:23421}, {x:100, y:456456}, {x:6, y:46}, {x:21, y:33}, {x:75, y:44}];//input array
         const expectedResult = [{ x: 6, y: 46 }, { x: 21, y: 33 }, { x: 34, y: 23421 }, { x: 75, y: 44 }, { x: 100, y: 456456 }];//what the array should be after sorting
